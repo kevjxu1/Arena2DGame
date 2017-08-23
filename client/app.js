2f179a4bb1020c78a9680d246420da6abe0e2c42
Kevin Jiarui Xu <kevjxu1@gmail.com>
2017-08-22 22:27:50 -0700

    client sends Globals to server

--- client/app.js
+++ client/app.js
@@ -1,103 +1,136 @@
 'use strict'; 
 
-var Globals = requre('/../globals');
+var player;
+var visiblePlayers = [];
+var socketId;
+
+var Globals = {
+    // game parameters
+    PLAYER_SIZE:  20,
+    PLAYER_SPEED: 2,
+    SCREEN_WIDTH: window.innerWidth * 0.7,
+    SCREEN_HEIGHT: window.innerHeight * 0.7,
+
+    // input constants
+    KEY_UNPRESSED: -1,
+    KEY_ENTER: 13,
+    KEY_LEFT: 37,
+    KEY_UP: 38,
+    KEY_RIGHT: 39,
+    KEY_DOWN: 40
+};
+
+player = {};
+player.name = '';
+player.color = 'red';
+player.x = Globals.SCREEN_WIDTH / 2;
+player.y = Globals.SCREEN_HEIGHT / 2;
 
 var IO = {
 	init: function() {
 		IO.socket = io.connect();
-		//IO.bindEvents();
+		IO.bindEvents();
 	},
+
+    bindEvents: function() {
+        //IO.socket.on('createPlayer', IO.createPlayer);
+        IO.socket.on('connect', function() {
+            socketId = IO.socket.socket.sessionid;
+        });
+        IO.socket.on("updatePlayer", IO.updatePlayer);
+    },
+
+    //createPlayer: function() {
+    //    player = {
+    //        name: '',
+    //        color:  'red',
+    //        x: SCREEN_WIDTH / 2,
+    //        y: SCREEN_HEIGHT / 2
+    //    };
+    //},
+    
+    updatePlayer: function(player) {
+        player = player;
+    }
 
 };
 
 var Input = {
-	keyPressed: Globals.KEY_UNPRESSED,
-
+    
 	addEventListeners: function() {
 		document.addEventListener("keydown", Input.onKeydown, false);
-		document.addEventListener("keyup", Input.onKeyup, false);
+		//document.addEventListener("keyup", Input.onKeyup, false);
 	},
 
 	onKeydown: function(e) {
-		Input.keyPressed = e.keyCode;
-		App.player.move(Input.keyPressed);
+        IO.socket.emit('movePlayer', { socket: socketId, key: e.keyCode, speed: Globals.PLAYER_SPEED });
 	},
 	
-	onKeyup: function(e) {
-		Input.keyPressed = Globals.KEY_UNPRESSED;
-		App.player.move(Input.keyPressed);
-	}
+	//onKeyup: function(e) {
+    //    IO.socket.emit(IO.socket, e.keyCode)
+	//}
 };
 
-var App = { 
+////////////////////////////////////////////////////
+// Draw on screen
+////////////////////////////////////////////////////
 
-    visiblePlayers: [],
+// helpers
 
-    Canvas:  {
+function clearCanvas(context) {
+    context.beginPath();
+    context.fillStyle = '#CCCFD3';  // light gray
+    context.fillRect(0, 0, Globals.SCREEN_WIDTH, Globals.SCREEN_HEIGHT);
+    context.closePath;
+}
 
-        clear: function(context) {
-            context.beginPath();
-            context.fillStyle = '#CCCFD3';  // light gray
-            context.fillRect(0, 0, Globals.CANVAS_WIDTH, Globals.CANVAS_HEIGHT);
-            context.closePath;
-        },
-
-        displayPlayer: function(context, player) {
-            context.beginPath();
-            context.rect(player.x, player.y, Globals.PLAYER_SIZE, Globals.PLAYER_SIZE);
-            context.fillStyle = player.color;
-            context.fill();
-            context.closePath();
-        },
-
-        displayPlayers: function(context) {
-            
-        },
-
-        mspf: 1,  // milliseconds per frame
-
-        loopDisplay: function(context) {
-			App.Canvas.clear(context);
-            App.Canvas.displayPlayer(context, App.player);
-            requestAnimationFrame(function () {
-				App.Canvas.loopDisplay(context);
-			});
-        }
-    },
-
-    player: {
-        name:  '',
-        color:  'red', 
-        x: Globals.CANVAS_WIDTH / 2,
-		y: Globals.CANVAS_HEIGHT / 2,
-
-		move: function(dir) {
-			switch (dir) {
-				case Globals.KEY_UP:
-					this.y -= Globals.PLAYER_SPEED;
-					break;
-				case Globals.KEY_RIGHT:
-					this.x += Globals.PLAYER_SPEED;
-					break;
-				case Globals.KEY_DOWN:
-					this.y += Globals.PLAYER_SPEED;
-					break;
-				case Globals.KEY_LEFT:
-					this.x -= Globals.PLAYER_SPEED;
-					break;
-				default:
-					break;
-			}
-		}
-    },
-
+function displayPlayer(context, player) {
+    context.beginPath();
+    context.rect(player.x, player.y, Globals.PLAYER_SIZE, Globals.PLAYER_SIZE);
+    context.fillStyle = player.color;
+    context.fill();
+    context.closePath();
 };
+
+
+// top modules
+
+function loopDisplay(context) {
+    clearCanvas(context);
+    displayPlayer(context, player);
+    requestAnimationFrame(function () {
+        loopDisplay(context);
+    });
+};
+
+function initCanvas(canvas) {
+    canvas = document.getElementById("canvas");
+    canvas.width = Globals.SCREEN_WIDTH;
+    canvas.height = Globals.SCREEN_HEIGHT;
+    context = canvas.getContext("2d");
+}
+
+////////////////////////////////////////////////////
+
+
+function createPlayer() {
+    //if (!IO.sockId)
+    //    return;
+    IO.socket.emit('addPlayer', { socketId: socketId, player: player });
+}
+
+function sendGlobals() {
+    IO.socket.emit('updateGlobals', Globals);
+}
 
 IO.init();
+sendGlobals(Globals);
+
 Input.addEventListeners();
-var canvas = document.getElementById("canvas");
-canvas.width = Globals.CANVAS_WIDTH;;
-canvas.height = Globals.CANVAS_HEIGHT;
-var context = canvas.getContext("2d");
-App.Canvas.loopDisplay(context);
+createPlayer();
+
+var canvas,
+    context;
+initCanvas(canvas);
+loopDisplay(context);
 
--- game.js
+++ game.js
@@ -1,21 +1,69 @@
 var io;
 var gameSocket;
+var Globals;
 
 Player = require('./player');
 var players = {};
+var sockets = {};
 
 module.exports = {
 
-    initGame:  function(sio. socket) {
+    initGame: function(sio, socket) {
         io = sio;    
         gameSocket = socket;
+		sockets[socket.id] = socket;
+		gameSocket.on('updateGlobals', updateGlobals);
         gameSocket.on('addPlayer', addPlayer);
+        gameSocket.on('movePlayer', movePlayer);
         console.log('game inited');
     },
 };
 
-function addPlayer(socket) {
-    players[socket.id] = new Player('', 'red', Globals.CANVAS_WIDTH / 2, Globals.CANVAS_HEIGHT / 2);
+function updateGlobals(data) {
+	console.log('updateGlobals');
+	console.log('data: ');
+	console.log(data);
+	Globals = data;
+}
+
+function addPlayer(msg) {
+	console.log('addPlayer');
+	console.log('msg: ');
+	console.log(msg);
+	let socketId = msg.socketId;
+	let player = msg.player;
+    //player = new Player('', 'red', Globals.CANVAS_WIDTH / 2, Globals.CANVAS_HEIGHT / 2);
+    player[socketId] = player;
+    //io.sockets.connected[socketId].emit('updatePlayer');
+	console.log(sockets);
+	sockets[socketId].emit('updatePlayer');
+}
+
+function movePlayer(data) {
+	console.log('movePlayer');
+	console.log('data: ');
+	console.log(data);
+    sockId = data.sockId;
+    dir = data.dir;
+    player = players[sockId];
+	switch (dir) {                                                         
+		case Globals.KEY_UP:                                               
+			player.y -= Globals.PLAYER_SPEED;                                
+			break;                                                         
+		case Globals.KEY_RIGHT:                                            
+			player.x += Globals.PLAYER_SPEED;                                
+			break;                                                         
+		case Globals.KEY_DOWN:                                             
+			player.y += Globals.PLAYER_SPEED;                                
+			break;                                                         
+		case Globals.KEY_LEFT:                                             
+			player.x -= Globals.PLAYER_SPEED;                                
+			break;                                                         
+		default:                                                           
+			break;                                                         
+	} 
+	//socket.emit('updatePlayer', player);
+    io.sockets.connected[socket.id].emit('updatePlayer', player);
 }
 
 
